3de84a8ed0168182e8c20e9785c3bfc4a8e98f13
emir <emir.jumaliev@gmail.com>
2019-09-23 09:26:31 -0400

    was made migration,added data,made fixtures

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name','description']
+    list_filter = ['category']
+    list_display_links = ['pk',]
+    search_fields = ['description']
+    fields = ['name','description', 'category','amount','price']
+
+
+admin.site.register(Product, ArticleAdmin)
